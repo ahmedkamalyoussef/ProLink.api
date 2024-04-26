f072c8dc6cb7097f035b2afbbeaa673ff56da7d4
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-04-26 21:56:19 +0300

    edit in unit of work

--- ProLink.Infrastructure/GenericRepository&UOW/UnitOfWork.cs
+++ ProLink.Infrastructure/GenericRepository&UOW/UnitOfWork.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Castle.Core.Resource;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProLink.Infrastructure.Data;
+using ProLink.Infrastructure.IGenericRepository_IUOW;
+using ProLink.Data.Entities;
 
 namespace ProLink.Infrastructure.GenericRepository_UOW
 {
     public class UnitOfWork
     {
+        private IDbContextTransaction transaction;
+
+        private AppDbContext _context;
+        public virtual IGenericRepository<User> User { get; set; }
+        public virtual IGenericRepository<FriendRequest> FriendRequest { get; set; }
+        public virtual IGenericRepository<JobRequest> JopRequest { get; set; }
+        public virtual IGenericRepository<Comment> Comment { get; set; }
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+
+        public void CreateTransaction()
+        {
+            transaction = _context.Database.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            transaction.Rollback();
+
+        }
+
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
     }
 }
