021d9b7221dd491e8663c55ff7e1fb63e6e0a76b
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-05-26 17:04:22 +0300

    add secret

---
+++ ProLink.api/ModuleSecretDependences.cs
@@ -0,0 +1,14 @@
+﻿using ProLink.Application.Mail;
+
+namespace ProLink.api
+{
+    public static class ModuleSecretDependences
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var mailingSection = configuration.GetSection("Mailing");
+            services.Configure<MailSettings>(mailingSection);
+            return services;
+        }
+    }
+}
