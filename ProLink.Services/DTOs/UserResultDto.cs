7f2a98c1c9e83e06e732ad7e9a1e212b6a435db1
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-06-02 21:03:43 +0300

    email

--- ProLink.Services/DTOs/UserResultDto.cs
+++ ProLink.Services/DTOs/UserResultDto.cs
@@ -7,6 +7,7 @@
         public bool IsFriend { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
         public string? JopTitle { get; set; }
         public string? CV { get; set; }
         public int? FollowersCount {  get; set; }
