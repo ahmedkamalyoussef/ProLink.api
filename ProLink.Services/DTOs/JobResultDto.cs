b4b9aba7c7bbac11d261d2dafb100bc4cd02d208
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-06-07 03:19:49 +0300

    add datetime

--- ProLink.Services/DTOs/JobResultDto.cs
+++ ProLink.Services/DTOs/JobResultDto.cs
@@ -6,6 +6,8 @@ namespace ProLink.Application.DTOs
     {
         public string Id { get; set; }
         public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+
         public bool IsRequestSent { get; set; }
         public bool IsAvailable { get; set; }
         public string Description { get; set; }
